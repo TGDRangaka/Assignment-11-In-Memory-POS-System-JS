78194eef985801414c677b1fd67093ace3f30ec7
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2023-10-08 23:56:56 +0530

    set ordered items to table

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,9 +2,28 @@ import {Order} from "../model/order.js";
 import {customers} from "../db/db_arrays.js";
 import {items} from "../db/db_arrays.js";
 import {orders} from "../db/db_arrays.js";
+import {Item} from "../model/item.js";
 
 let cusRowIndex = null;
 let itemRowIndex = null;
+let total = 0;
+let subTotal = 0;
+let tempItems = [];
+let addedItems = [];
+
+const loadAddItemTable = ()=>{
+    addedItems.map((item) => {
+        $("#orderedItemTBody").append(`
+                    <tr>
+                        <td> ${item.id} </td>
+                        <td> ${item.name} </td>
+                        <td> ${item.price} </td>
+                        <td> ${item.qty} </td>
+                        <td> ${item.price * item.qty} </td>
+                    </tr>
+    `   );
+    })
+}
 
 //loadCustomers
 $("#orderCusId>button").on('click', ()=> {
@@ -43,4 +62,23 @@ $("#itemDropdown").on('click', "a", function(){
 });
 
 //add-item action
+$("#add-item-btn").on('click', ()=>{
+    let id = $("#orderItemId>button").text(),
+        name = $("#orderItemName").val(),
+        price = Number.parseFloat($("#orderItemPrice").val()),
+        qty = Number.parseInt($("#orderItemQty").val()),
+        itemTotal = price * qty;
+
+
+    addedItems.push(new Item(id, name, price, qty, itemTotal));
+    loadAddItemTable();
+
+    tempItems.push(items[itemRowIndex]);
+    items[itemRowIndex].qty -= qty;
+
+    subTotal += itemTotal;
+    $("#subTotal").text(`Sub Total: Rs. ${subTotal} `)
+});
+
+
 //save order
